f0d6f4ea22b0a53ea7e36b4ea83704448614a62c
Takahito Nakano <kano@ponko2.jp>
2015-06-06 16:16:27 +0900

    リファクタリング

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 'use strict';
 
 var gulp        = require('gulp');
-var browserSync = require('browser-sync');
+var browserSync = require('browser-sync').create();
 var _           = require('underscore');
 var elixir      = require('laravel-elixir');
 var config      = elixir.config;
+
+/**
+ * Build the BrowserSync Gulp task.
+ *
+ * @param {array|null} options
+ */
+var buildTask = function(options) {
+  gulp.task('browser-sync', function() {
+    if (browserSync.active === true) {
+      browserSync.reload();
+    } else if (gulp.tasks.watch.done === true) {
+      browserSync.init(options);
+    }
+  });
+};
 
 elixir.extend('browserSync', function(src, options) {
   var defaultSrc = [
@@ -13,16 +28,12 @@ elixir.extend('browserSync', function(src, options) {
     'resources/views/**/*'
   ];
 
-  gulp.task('browser-sync', function() {
-    if (browserSync.active === true) {
-      browserSync.reload();
-    } else if (gulp.tasks.watch.done === true) {
-      browserSync(_.extend({
-        notify: false,
-        proxy: 'homestead.app'
-      }, options));
-    }
-  });
+  var defaultOptions = {
+    notify: false,
+    proxy: 'homestead.app'
+  };
+
+  buildTask(_.extend(defaultOptions, options));
 
   if (config.production === false) {
     this.registerWatcher('browser-sync', src || defaultSrc);
